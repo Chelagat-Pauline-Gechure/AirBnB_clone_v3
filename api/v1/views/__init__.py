ea3047a616bcbff5218d27270690f4366669f4df
Chelagat-Pauline-Gechure <cpaulineg@gmail.com>
2023-10-30 06:45:50 +0000

    6. State

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -6,5 +6,13 @@ from flask import Blueprint
 """ Create a Blueprint instance with a URL prefix """
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
-""" Wildcard import of everything in the package api.v1.views.index. """
-from api.v1.views.index import *
+""" Wildcard import of everything in the package api.v1.views. """
+if app_views is not None: 
+    from api.v1.views.index import *
+    from api.v1.views.states import *
+    from api.v1.views.cities import *
+    from api.v1.views.amenities import *
+    from api.v1.views.users import *
+    from api.v1.views.places import *
+    from api.v1.views.places_reviews import *
+    from api.v1.views.places_amenities import *
