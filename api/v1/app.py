ac75ce5bb94f65c86c535f08784b9788e27def26
Chelagat-Pauline-Gechure <cpaulineg@gmail.com>
2023-10-30 06:30:28 +0000

    5. Not found & 12. HTTP access control (CORS)

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,8 @@
 This module contains the principal application
 """
 
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
 
 from models import storage
 from api.v1.views import app_views
@@ -16,12 +17,20 @@ app = Flask(__name__)
 """" Register the app_views blueprint to the Flask instance """
 app.register_blueprint(app_views)
 
+""" Create CORS instance """
+CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
+
 """ Define a method to handle teardown_appcontext """
 @app.teardown_appcontext
 def close_storage(exception):
     """ Teardown method to close the storage """
     # print(exception)
     storage.close()
+
+@app.errorhandler(404)
+def not_found_404(err):
+    """Handles 404 HTTP error code."""
+    return jsonify({"error": "Not found"}), 404
 
 if __name__ == "__main__":
     """Get the host and port from environment variables or use defaults. """
