df5767499faf758f73c2b6a10538f4ea6c6a4226
Chelagat-Pauline-Gechure <cpaulineg@gmail.com>
2023-10-30 06:04:24 +0000

    4. Some stats?

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -15,3 +15,15 @@ def get_api_status():
     Returns: JSON response indicating the status of the API
     """
     return jsonify({"status": "OK"})
+
+@app_views.route("/stats", methods=["GET"], strict_slashes=False)
+def stat():
+    """
+    Retrieves the number of each objects by type
+    """
+    return jsonify({"amenities": storage.count("Amenity"),
+                    "cities": storage.count("City"),
+                    "places": storage.count("Place"),
+                    "reviews": storage.count("Review"),
+                    "states": storage.count("State"),
+                    "users": storage.count("User")})
