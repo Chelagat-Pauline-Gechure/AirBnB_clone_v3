cadcda44c304f33472fee29d064569a9158d4b31
gkiplangat <gideonkiplangat4@gmail.com>
2023-10-30 14:13:27 +0000

    feat:update api

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,29 +1,32 @@
 #!/usr/bin/python3
-""" 
-This module defines routes for the index of the API.
-The main route `/status` is used to check the status of the API.
-"""
-
-from models import storage
+"""Contains the index view for the API."""
 from flask import jsonify
+
 from api.v1.views import app_views
+from models import storage
+
 
 @app_views.route("/status", methods=["GET"], strict_slashes=False)
-def get_api_status():
-    """
-    Retrieves the status of the API
-    Returns: JSON response indicating the status of the API
-    """
-    return jsonify({"status": "OK"})
+def status():
+    """Returns the status of the API"""
+    return jsonify(status="OK")
+
 
 @app_views.route("/stats", methods=["GET"], strict_slashes=False)
 def stat():
-    """
-    Retrieves the number of each objects by type
-    """
-    return jsonify({"amenities": storage.count("Amenity"),
-                    "cities": storage.count("City"),
-                    "places": storage.count("Place"),
-                    "reviews": storage.count("Review"),
-                    "states": storage.count("State"),
-                    "users": storage.count("User")})
+    """Returns the number of all objects by type"""
+    from models.amenity import Amenity
+    from models.city import City
+    from models.place import Place
+    from models.review import Review
+    from models.state import State
+    from models.user import User
+
+    return jsonify(
+        amenities=storage.count(Amenity),
+        cities=storage.count(City),
+        places=storage.count(Place),
+        reviews=storage.count(Review),
+        states=storage.count(State),
+        users=storage.count(User),
+    )
